c8250f8e3603236b9dd234b875e70fb226242baa
Anastasia Marie <ana.goodwin10@gmail.com>
2017-06-02 00:09:54 -0500

    about page redo

--- public_html/wp-content/themes/alacode/page-services.php
+++ public_html/wp-content/themes/alacode/page-services.php
@@ -12,56 +12,47 @@ Template Name: Services Template
 <!-- Two Columns -->
 <div class="row two-columns">
 	<main class= "about-main">
+		
+		<img class="banner-image" src="http://alacode.io/wp-content/uploads/2017/06/bannerImage.jpg">
 
-			<section class="services-what-we-do">
-				<div class="col-md-4 text-center">
-					<img class="services-icon" src="http://i36.photobucket.com/albums/e26/anagoodwin/coneIcon_zpscwhixp4z.png">
-					<p class="about-text text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum mollitia necessitatibus commodi adipisci voluptatem fugiat soluta delectus doloribus aperiam quam, consequatur ipsam nulla, pariatur? Fuga quis modi necessitatibus cum commodi.
-					</p>
-				</div>
-				<div class="col-md-4 text-center">
-					<img class="services-icon" src="http://i36.photobucket.com/albums/e26/anagoodwin/coneScoopIcon_zps4x5qmrvx.png">
-					<p class="about-text text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum mollitia necessitatibus commodi adipisci voluptatem fugiat soluta delectus doloribus aperiam quam, consequatur ipsam nulla, pariatur? Fuga quis modi necessitatibus cum commodi.
-					</p>
-				</div>
-				<div class="col-md-4 text-center">
-					<img class="services-icon" src="http://i36.photobucket.com/albums/e26/anagoodwin/coneFinishedIcon_zpsgsz2or1z.png">
-					<p class="about-text text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum mollitia necessitatibus commodi adipisci voluptatem fugiat soluta delectus doloribus aperiam quam, consequatur ipsam nulla, pariatur? Fuga quis modi necessitatibus cum commodi.
-					</p>
-				</div>
-			</section>
-
-			<hr class="body-divider">
-
-			<hr class="body-divider">
-
-			<section class="services-testimonials">
-				<div class="row">
+			<section>
+				<div class="row team-about-section">
 					<div class="col-md-6 text-center">
-						<img src="http://fillmurray.com/400/300" alt="team photo">
+						<img src="http://alacode.io/wp-content/uploads/2017/06/sprinkleconesfriends.jpg" alt="team photo">
 					</div>
-					<div class="col-md-6 text-left team-about-text">
-						<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit alias, dolore explicabo fuga molestias voluptate vel vero libero repellat sapiente. Omnis, aliquam consequuntur sapiente, cumque sequi sunt assumenda sint quidem! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores commodi modi nemo quidem, explicabo a eaque doloribus voluptatibus provident, necessitatibus velit voluptates minus, fugit. Quod necessitatibus rerum voluptates ut sit.
+					<div class="col-md-4 team-about-text">
+						<p class="team-note"> So Ann, the question is, do you want a man or a boy? I know how I would answer. Did you enjoy your lunch, mom? You drank it fast enough. Don't leave your Uncle T-bag hanging. Yeah, that's a cultural problem is what it is. You know, your average American male is in a perpetual state of adolescence, you know, arrested development. (Hey. That's the name of the show!) How am I supposed to find someone willing to go into that musty old claptrap? It's, like, Hey, you want to go down to the whirlpool? Yeah, I don't have a husband. I call it Swing City.</p>
+						<p>
+						You might wanna lean away from that fire since you're soaked in alcohol. In fact, it was a box of Oscar's legally obtained medical marijuana. Primo bud. Real sticky weed. We need a name. Maybe "Operation Hot Mother." No, let's try to top that. (They never did.)
 						</p>
+						<p>
+						They frame my junk. Did Ted make an appointment? No. Well, then Ted can GET THE HELL OUT OF THIS OFFICE! YOU GET THE HELL OUT! If I wanted something your thumb touched I'd eat the inside of your ear.
+						</p>
+						<p>
+						Of course. The "Bob Loblaw Law Blog." Wow. You, sir, are a mouthful! Michael was concerned that he was caught in a lie about his family. The family was concerned that they were being confronted by a woman they had clubbed, drugged, and left on a bench. This objectification of women has to stop. It's just Mom and whores. I figured out a way to make money while I'm working!</p>
+						<p>
+						And although the intervention didn't work, it turned into one of the Bluth family's better parties. Turns out he ended up getting too friendly with the teddy bear. Oh by the way, Doctor said no kissing her on the face for one week. I was like make it two weeks, see if I care!
+						</p>
+
+						<span class="signature pull-right">ana &amp; jillian</span>
 					</div>
 				</div>
 			</section>
 
 			<hr class="body-divider">
+			
+			<h2 class="text-center services-section-header">Meet our team!</h2>
 
 			<section class="individual-about-section">
 				<div class="col-md-4 text-center">
-					<img src="http://fillmurray.com/200/200">
+					<img class="image-circle profile-pic" src="http://alacode.io/wp-content/uploads/2017/06/Ana.jpg">
+					<h3>Ana</h3>
 					<p class="about-text text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum mollitia necessitatibus commodi adipisci voluptatem fugiat soluta delectus doloribus aperiam quam, consequatur ipsam nulla, pariatur? Fuga quis modi necessitatibus cum commodi.
 					</p>
 				</div>
 				<div class="col-md-4 text-center">
-					<img src="http://fillmurray.com/200/200">
-					<p class="about-text text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum mollitia necessitatibus commodi adipisci voluptatem fugiat soluta delectus doloribus aperiam quam, consequatur ipsam nulla, pariatur? Fuga quis modi necessitatibus cum commodi.
-					</p>
-				</div>
-					<div class="col-md-4 text-center">
-					<img src="http://fillmurray.com/200/200">
+					<img class="image-circle profile-pic" src="http://alacode.io/wp-content/uploads/2017/06/Jillian.jpg">
+					<h3>Jillian</h3>
 					<p class="about-text text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum mollitia necessitatibus commodi adipisci voluptatem fugiat soluta delectus doloribus aperiam quam, consequatur ipsam nulla, pariatur? Fuga quis modi necessitatibus cum commodi.
 					</p>
 				</div>
